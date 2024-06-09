8b80c1d521e9dfce89d888f4b15261a006f5fd85
2018331109 <ahmadulsh@gmail.com>
2024-06-09 17:11:43 +0600

    using exists

--- fs-module/index.js
+++ fs-module/index.js
@@ -1,7 +1,12 @@
 console.log("hi");
 const fs=require('fs');
 
-fs.unlink('file2.txt', (err, data)=>{
-    if(err) console.log(err);
-    else console.log(data);
+fs.writeFileSync('file2.txt', 'This is a file', (err)=>{
+    if(err) console.log("error");
+    else console.log("successful");
+})
+
+fs.exists('file2.txt', (res)=>{
+    if(res) console.log("exists");
+    else console.log("don't exists");
 })
