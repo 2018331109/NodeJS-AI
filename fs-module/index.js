8295d062a7ad29402c560a119fc9d40a042092cd
2018331109 <ahmadulsh@gmail.com>
2024-06-09 16:01:37 +0600

    using arrow function

--- fs-module/index.js
+++ fs-module/index.js
@@ -1,7 +1,7 @@
 console.log("hi");
 const fs=require('fs');
 
-fs.writeFile('file1.txt', "This is file 1", function(err){
+fs.writeFile('file1.txt', "This is file 1", (err)=>{
     if(err) console.log(err);
     else console.log("successfull");
 })
