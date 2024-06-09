53db60ef78d79f0d2d48dd3c7357b0a7f5626048
2018331109 <ahmadulsh@gmail.com>
2024-06-09 15:25:19 +0600

    exporting during declaration

---
+++ import-export/index.js
@@ -0,0 +1,7 @@
+console.log("hello world");
+
+//const s1 = require('./student.js');
+const {getName, getAge} = require('./student.js')
+
+console.log(getName()); // Output: Ahmadul Hasan
+console.log(getAge());  // Output: X-24
---
+++ import-export/student.js
@@ -0,0 +1,9 @@
+exports.getName = () => {
+  return "Ahmadul Hasan";
+};
+
+exports.getAge = () => {
+  return "X-24";
+};
+
+//module.exports = { getName, getAge };
